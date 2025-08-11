dbbda01a3712d96dc88cdfa34fc3aa06f4ef8198
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-08-11 20:43:08 +0530

    feat: implement CreatePage with product input fields and styling

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
 
-export default CreatePage
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
+  // Use the same background gradient as App.jsx
+  const pageBg = useColorModeValue(
+    "linear(to-br, purple.200, blue.200)", // light mode
+    "linear(to-br, #273131ff, #345757ff, #247474ff, #169292ff)"    // dark mode
+  );
+
+  return (
+    <Box minH="100vh" >
+      <Container maxW={"container.sm"} py={12}>
+        <VStack spacing={8}>
+          <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+            Create New Product
+          </Heading>
+          <Box
+            w={"full"}
+            bg={useColorModeValue("white", "gray.800")}
+            p={6}
+            rounded={"lg"}
+            shadow={"md"}bgGradient={pageBg}
+          >
+            <VStack spacing={4}>
+              <Input
+                placeholder="Product Name"
+                name="name"
+                value={newProduct.name}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, name: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Price"
+                name="price"
+                value={newProduct.price}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, price: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Image URL"
+                name="image"
+                value={newProduct.image}
+                onChange={(e) =>
+                  setNewProduct({ ...newProduct, image: e.target.value })
+                }
+              />
+              <Button colorScheme="blue" onClick={handleAddProduct} w={"full"}>
+                Add Product
+              </Button>
+            </VStack>
+          </Box>
+        </VStack>
+      </Container>
+    </Box>
+  );
+};
+
+export default CreatePage;
