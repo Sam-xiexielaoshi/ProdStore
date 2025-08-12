f36f39cd1c852086e0da98ab63ad3b6e8de5342d
Sam-xiexielaoshi <samagrasingh20032003@gmail.com>
2025-08-12 22:14:04 +0530

    feat: add Zustand for state management and implement product CRUD operations with ProductCard and CreatePage components

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -5,9 +5,11 @@ import {
   Heading,
   Input,
   useColorModeValue,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useProductStore } from "../store/product";
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -16,18 +18,40 @@ const CreatePage = () => {
     image: "",
   });
 
-  const handleAddProduct = () => {
-    console.log(newProduct);
+  const toast = useToast();
+
+  const { createProduct } = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    setNewProduct({ name: "", price: "", image: "" }); // Reset form after submission
   };
 
   // Use the same background gradient as App.jsx
   const pageBg = useColorModeValue(
     "linear(to-br, purple.200, blue.200)", // light mode
-    "linear(to-br, #273131ff, #345757ff, #247474ff, #169292ff)"    // dark mode
+    "linear(to-br, #273131ff, #345757ff, #247474ff, #169292ff)" // dark mode
   );
 
   return (
-    <Box minH="100vh" >
+    <Box minH="100vh">
       <Container maxW={"container.sm"} py={12}>
         <VStack spacing={8}>
           <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
@@ -38,7 +62,8 @@ const CreatePage = () => {
             bg={useColorModeValue("white", "gray.800")}
             p={6}
             rounded={"lg"}
-            shadow={"md"}bgGradient={pageBg}
+            shadow={"md"}
+            bgGradient={pageBg}
           >
             <VStack spacing={4}>
               <Input
